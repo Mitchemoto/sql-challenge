307345fe6b1907617e0f00dd96df3bb2bf40415b
Mitchell <erich.mitchell87@gmail.com>
2021-02-22 22:36:55 -0700

    queries 3-6

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -9,3 +9,29 @@ order by last_name,first_name asc;
 select first_name, last_name, hire_date
 from employees
 where hire_date like '%1986';
+
+--List the manager of each department with the following information: 
+-- department number, department name, the manager's employee number, last name, first name.
+select departments.dept_no,departments.dept_name,dept_manager.emp_no,employees.last_name,employees.first_name
+from dept_manager
+join departments on dept_manager.dept_no=departments.dept_no
+join employees on dept_manager.emp_no=employees.emp_no;
+
+--List the department of each employee with the following information: 
+-- employee number, last name, first name, and department name.
+select employees.emp_no,employees.last_name,employees.first_name, departments.dept_name
+from employees
+join dept_emp on employees.emp_no=dept_emp.emp_no
+join departments on  dept_emp.dept_no=departments.dept_no
+
+--List first name, last name, and sex for employees whose 
+--first name is "Hercules" and last names begin with "B."
+select first_name, last_name, sex 
+from employees
+where first_name = 'Hercules' and last_name like 'B%';
+
+--List all employees in the Sales department, including their 
+--employee number, last name, first name, and department name.
+
+
+

541c51f59619ef7232690f094e3e80ef33867ef0
Mitchell <erich.mitchell87@gmail.com>
2021-02-22 22:16:55 -0700

    #1 and #2 complete

--- EmployeeSQL/employeetables.sql
+++ EmployeeSQL/employeetables.sql
@@ -8,17 +8,46 @@ drop table if exists titles;
 
 --create the departments table
 create table departments (
-	dept_no	varchar,
+	dept_no	varchar primary key,
 	dept_name varchar
 );
 
 -- integrity check
 select * from departments;
 
+--create titles table
+create table titles(
+	title_id varchar primary key,
+	title varchar
+);
+
+-- integrity check
+select * from titles
+order by title asc;
+
+--create employees table
+create table employees(
+	emp_no varchar primary key,
+	emp_title_id varchar,
+	foreign key (emp_title_id) references titles(title_id),
+	birth_date date,
+	first_name varchar,
+	last_name varchar,
+	sex	varchar,
+	hire_date varchar
+);
+
+-- integrity check
+select * from employees
+order by last_name,first_name asc;
+
+
 --create the dept_emp table
 create table dept_emp(
 	emp_no varchar,
-	dept_no varchar
+	foreign key (emp_no) references employees(emp_no),
+	dept_no varchar,
+	foreign key (dept_no) references departments(dept_no)
 );
 
 -- integrity check
@@ -27,62 +56,23 @@ select * from dept_emp;
 --create dept_manager table
 create table dept_manager(
 	dept_no varchar,
-	emp_no varchar
+	foreign key (dept_no) references departments(dept_no),
+	emp_no varchar,
+	foreign key (emp_no) references employees(emp_no)
 	);
 	
 -- integrity check
 select * from dept_manager;
 
---create employees table
-create table employees(
-	emp_no varchar,
-	emp_title_id varchar,
-	birth_date date,
-	first_name varchar,
-	last_name varchar,
-	sex	varchar,
-	hire_date date
-);
-
--- integrity check
-select * from employees
-order by last_name,first_name asc;
-
 --create salaries table
 create table salaries(
 	emp_no varchar,
-	slary int
+	foreign key (emp_no) references employees(emp_no),
+	salary int
 );
 
 -- integrity check
 select * from salaries;
 
---create titles table
-create table titles(
-	title_id varchar,
-	title varchar
-);
 
--- integrity check
-select * from titles
-order by title asc;
 
---from data mapper create the relationships
-
--- ALTER TABLE "Department Employee" ADD CONSTRAINT "fk_Department Employee_emp_no" FOREIGN KEY("emp_no")
--- REFERENCES "Employees" ("emp_no");
-
--- ALTER TABLE "Department Employee" ADD CONSTRAINT "fk_Department Employee_dept_no" FOREIGN KEY("dept_no")
--- REFERENCES "Departments" ("dept_no");
-
--- ALTER TABLE "Department Manager" ADD CONSTRAINT "fk_Department Manager_dept_no" FOREIGN KEY("dept_no")
--- REFERENCES "Departments" ("dept_no");
-
--- ALTER TABLE "Department Manager" ADD CONSTRAINT "fk_Department Manager_emp_no" FOREIGN KEY("emp_no")
--- REFERENCES "Employees" ("emp_no");
-
--- ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
--- REFERENCES "Employees" ("emp_no");
-
--- ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_title_id" FOREIGN KEY("title_id")
--- REFERENCES "Employees" ("emp_title_id");
